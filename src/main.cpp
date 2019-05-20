e9b8fc8f7cabc42d361f6480640b4d5a88b3580c
Rohit Singh <singhrohit2@hotmail.com>
2019-05-20 13:30:06 -0600

    Update scene

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,16 @@
 
 int main() {
     Scene scene;
-    scene.meshes.push_back(Mesh((char*)"obj/cow.obj", 1.5, 0, 1, 0));
-    scene.meshes.push_back(Mesh((char*)"obj/plane.obj", 4, 1, 1, 1));
-    scene.lights.push_back(Light(Vec3(1,0,-3), Vec3(1,1,1)));
-    scene.lights.push_back(Light(Vec3(-1,0,-3), Vec3(1,1,1)));
-    scene.lights.push_back(Light(Vec3(0,0,-3), Vec3(1,1,1)));
+    scene.meshes.push_back(Mesh((char *)"obj/cow.obj", 1.5, 0.03, 1, 1));
+    scene.meshes.push_back(Mesh((char *)"obj/plane.obj", 4, 0.25, 1, 1));
+    for (int i = -9; i < 10; i++) {
+        scene.lights.push_back(Light(Vec3(i, 5, i), Vec3(1, 1, 1)));
+    }
+    scene.lights.push_back(Light(Vec3(1, 0, -3), Vec3(10, 0, 0)));
+    scene.lights.push_back(Light(Vec3(-1, 0, -3), Vec3(0, 10, 0)));
+    scene.lights.push_back(Light(Vec3(0, 0, -3), Vec3(0, 0, 10)));
     scene.camera = Camera();
-    Canvas canvas(1024, 1024);
+    Canvas canvas(80, 80);
     render(canvas, scene);
-    canvas.write_ppm((char*)"img.ppm");
+    canvas.write_ppm((char *)"img.ppm");
 }
